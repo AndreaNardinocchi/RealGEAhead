f0febb34f33594dbe9491eda90c90530be639dc5
AndreaNardinocchi <andrea.nardinocchi76@gmail.com>
2026-01-17 10:19:26 +0000

    Add app and toolbar with dummy data to Search Results page

--- frontend/src/pages/searchResultsPage.tsx
+++ frontend/src/pages/searchResultsPage.tsx
@@ -1,4 +1,7 @@
-import { Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+// https://mui.com/material-ui/material-icons/
+import HotelIcon from "@mui/icons-material/Hotel";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import React, { useEffect } from "react";
 
 /**
@@ -16,6 +19,25 @@ const SearchResultsPage: React.FC = () => {
 
   return (
     <>
+      {/* Top App Bar indicating the search dates*/}
+      <AppBar position="static" sx={{ backgroundColor: "#EFF5E0" }}>
+        <Toolbar>
+          <HotelIcon sx={{ mr: 1, color: "#000000de" }} />
+
+          <Typography variant="h6" sx={{ flexGrow: 1, color: "#000000de" }}>
+            Search Results
+          </Typography>
+
+          {/* Show selected dates + guests */}
+          <Box display="flex" alignItems="center" gap={1}>
+            <CalendarMonthIcon sx={{ color: "#000000de" }} />
+            <Typography variant="body2" sx={{ color: "#000000de" }}>
+              {/* These are placeholder data */}
+              2026-01-14 → 2026-01-14 (2 guests)
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
       {/* Page content goes here */}
       <Typography
         variant="h4"
@@ -23,6 +45,18 @@ const SearchResultsPage: React.FC = () => {
         style={{ padding: "2rem", textAlign: "center" }}
       >
         Search Results page under construction
+      </Typography>
+      <Typography
+        variant="body2"
+        style={{ padding: "2rem", textAlign: "center", fontWeight: "bold" }}
+      >
+        Room Card goes here
+      </Typography>
+      <Typography
+        variant="body2"
+        style={{ padding: "2rem", textAlign: "center", fontWeight: "bold" }}
+      >
+        Room Card goes here
       </Typography>
     </>
   );
